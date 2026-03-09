aa91ab887a0ffc8ab590edf169043ba1d7625857
Ronaldo Ferreira da Silva <ronaldofsilva1@gmail.com>
2026-03-09 18:14:48 -0300

    atualização para aula 09-03-2026

--- 01_nodejs-intro/server.js
+++ 01_nodejs-intro/server.js
@@ -1,35 +1,7 @@
-import app from './src/app.js';
+import app from "./src/app.js";
 
-const people = [
-    { "id": 1, "name": "Maria dos Santos", "age": 40 },
-    { "id": 2, "name": "José dos Santos", "age": 30 }
-];
+const PORT = 3000;
 
-/** GET: Lista todos os usuários cadastrados */
-app.get("/people", (req, res) => {
-    res.status(200).json(people);
-});
-
-/** POST: Cria um novo usuário e o adiciona à lista */
-app.post("/people/create", (req, res) => {
-    const data = req.body;
-    people.push(data);
-    res.status(201).json({
-        "error": false,
-        "msg": "Registro criado com sucesso!",
-        "data": data
-    });
-});
-
-/** PUT: Atualiza o nome e a idade de um usuário existente pelo ID (índice) */
-app.put("/people/:id", (req, res) => {
-    const index = req.params.id;
-    people[index - 1].name = req.body.name;
-    people[index - 1].age = req.body.age;
-    res.status(200).json(`${req.body.name} alterado com sucesso.`);
-});
-
-/** DELETE: Remove um usuário da lista com base no ID fornecido */
-app.delete("/people/delete/:id", (req, res) => {
-    res.status(200).send("Registro excluído com sucesso");
+app.listen(PORT, () => {
+  console.log("Servidor escutando!");
 });
--- 01_nodejs-intro/src/app.js
+++ 01_nodejs-intro/src/app.js
@@ -1,8 +1,46 @@
-import express from 'express';
-const PORT = 3000;
+import express from "express";
+
+const people = [
+    { "id": 1, "name": "Maria dos Santos", "age": 40 },
+    { "id": 2, "name": "José dos Santos", "age": 30 }
+];
+
 const app = express();
-app.use(express.json())
-app.listen(PORT, () => {
-    console.log("Servidor escutando");
+
+app.use(express.json());
+
+/** GET: Lista todos os usuários cadastrados */
+app.get("/people", (req, res) => {
+    res.status(200).json(people);
 });
+
+
+/** POST: Cria um novo usuário e o adiciona à lista */
+app.post("/people/create", (req, res) => {
+    const data = req.body;
+    people.push(data);
+    res.status(201).json({
+        "error": false,
+        "msg": "Registro criado com sucesso!",
+        "data": data
+    });
+});
+
+/** PUT: Atualiza o nome e a idade de um usuário existente pelo ID (índice) */
+app.put("/people/:id", (req, res) => {
+    const index = req.params.id;
+    people[index - 1].name = req.body.name;
+    people[index - 1].age = req.body.age;
+    res.status(200).json(`${req.body.name} alterado com sucesso.`);
+});
+
+/** DELETE: Remove um usuário da lista com base no ID fornecido */
+app.delete("/people/:id", (req, res) => {
+    const index = buscaPessoa(req.params.id);
+    people.splice(index, 1);
+    res.status(200).send("Registro excluído com sucesso");
+});
+
+
+
 export default app;
