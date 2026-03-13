af773d156cd4ccb785181cfeb0f353c482f1e229
Ronaldo Ferreira da Silva <ronaldofsilva1@gmail.com>
2026-03-13 15:12:49 -0300

    Aula do dia 09-03-2026

--- 01_nodejs-intro/src/app.js
+++ 01_nodejs-intro/src/app.js
@@ -11,6 +11,7 @@ app.use(express.json());
 
 /** GET: Lista todos os usuários cadastrados */
 app.get("/people", (req, res) => {
+    res.set('Cache-Control','public, max-age=3600');
     res.status(200).json(people);
 });
 
@@ -28,19 +29,53 @@ app.post("/people/create", (req, res) => {
 
 /** PUT: Atualiza o nome e a idade de um usuário existente pelo ID (índice) */
 app.put("/people/:id", (req, res) => {
-    const index = req.params.id;
-    people[index - 1].name = req.body.name;
-    people[index - 1].age = req.body.age;
+    const index = buscaPessoa(req.params.id);
+    if (index == -1){
+        res.status(404).json(`Pessoa não econtrada.`);
+    }
+    people[index].name = req.body.name;
+    people[index].age = req.body.age;
     res.status(200).json(`${req.body.name} alterado com sucesso.`);
+});
+
+app.get('/people/search',(req, res) => {
+  const name = req.query.name;
+  try{
+    const person = buscaPessaPorNome(name);
+    if (!person){
+        res.status(404).json('Pessoa não encontrada');
+    }
+    res.status(200).json(person);
+  }catch(erro){
+    res.status(500).json(`Erro interno: ${erro.message}`);
+  }
 });
 
 /** DELETE: Remove um usuário da lista com base no ID fornecido */
 app.delete("/people/:id", (req, res) => {
     const index = buscaPessoa(req.params.id);
-    people.splice(index, 1);
-    res.status(200).send("Registro excluído com sucesso");
+    try{
+        if (index == -1){
+            res.status(404).json('Pessoa não encontrada');
+        }
+        people.splice(index, 1);
+        res.status(200).send("Registro excluído com sucesso");
+    }catch(erro){
+        res.status(500).json(`Erro interno: ${erro.message}`);
+    }
+
 });
 
+function buscaPessoa(id){
+    return people.findIndex(person => 
+        person.id === Number(id)
+    );
+}
 
+function buscaPessaPorNome(nome){
+    return people.find(person =>
+        person.name === nome
+    );
+}
 
 export default app;
